aee5d642ba1dc9b17c5df2ddcc6ed3d7fccd0e24
Aman Verma <vaman644@gmail.com>
2021-10-10 16:34:39 +0530

    Deleting items from custom toDo lists and installing lodash

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const _ = require("lodash");
 
 const app = express();
 
@@ -9,7 +10,6 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-const workItems = [];
 
 mongoose.connect("mongodb://localhost:27017/todoListDB");
 
@@ -59,7 +59,7 @@ app.get("/", function (req, res) {
 
 
 app.get("/:customListName", function(req,res){
-  const customListName = req.params.customListName;
+  const customListName = _.capitalize(req.params.customListName);
 
   List.findOne({name: customListName}, function(err,foundList){
     if(!err){
@@ -106,14 +106,25 @@ app.post("/", function (req, res) {
 
 app.post("/delete", function(req,res){
   const checkedItemId = req.body.checkbox;
-  Item.findByIdAndRemove(checkedItemId, function(err){
-    if(err){
-      console.log(err);
-    }else{
-      console.log("Item Successfully Removed");
-      res.redirect("/");
-    }
-  });
+  const listName = req.body.listItem;
+
+  console.log(listName);
+  if(listName === "Today"){
+    Item.findByIdAndRemove(checkedItemId, function(err){
+      if(err){
+        console.log(err);
+      }else{
+        console.log("Item Successfully Removed");
+        res.redirect("/");
+      }
+    });
+  }else{
+    List.findOneAndUpdate({name: listName}, {$pull: {items: {_id: checkedItemId}}}, function(err,result){
+      if(!err){
+        res.redirect("/" + listName);
+      }
+    });
+  }
 })
 
 app.get("/work", function (req, res) {
